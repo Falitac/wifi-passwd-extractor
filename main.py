0d3af07919831ad2a412c9766179e97cf2142d20
Falitac <fastkonrad@gmail.com>
2020-11-09 10:30:20 +0100

    slight change

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 import subprocess
 
 
+def main():
+	data = get_netsh_wifis_data()
+	wifis = get_wifi_names(data)
+	for wifi in wifis:
+		print_wifi_password(wifi)
+
+
+def print_wifi_password(wifi):
+	data = get_netsh_password_data(wifi)
+	wifi_password = get_wifi_password(data)
+	try:
+		print(f'Wi-fi: {wifi}, Password: {wifi_password[0]}')
+	except Exception as e:
+		print(f'Wi-fi: {wifi}, Password cannot be read')
+
+
 def get_netsh_wifis_data():
 	return subprocess.check_output(['netsh', 'wlan', 'show', 'profiles']).decode('utf-8').split('\n')
 
@@ -15,24 +31,6 @@ def get_netsh_password_data(wifi):
 
 def get_wifi_password(data):
 	return [line.split(':')[1][1:-1] for line in data if 'Key Content' in line]
-
-
-def print_wifi_password(wifi):
-	data = get_netsh_password_data(wifi)
-	wifi_password = get_wifi_password(data)
-	try:
-		print(f'Wi-fi: {wifi}, Password: {wifi_password[0]}')
-	except Exception as e:
-		print(f'Wi-fi: {wifi}, Password cannot be read')
-
-
-
-def main():
-	data = get_netsh_wifis_data()
-	wifis = get_wifi_names(data)
-
-	for wifi in wifis:
-		print_wifi_password(wifi)
 
 
 if __name__ == '__main__':
